b263c02aae791ebe5230191f45beb92824027296
Rubén Villar Grela <ruben@rubenvillar.net>
2019-05-21 19:12:50 +0200

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Button>Hello world</Button>
     </div>
   );
 }
